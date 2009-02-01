8fc9cbb5ddc57f5573ec021ab839c46b4f92301b
Alan Moore <amkimian@mac.com>
2009-02-01 09:44:41 -0800

    Adding the table

--- src/util_emxtype.erl
+++ src/util_emxtype.erl
@@ -15,5 +15,26 @@ create_type(Type) when is_record(Type, emxtypeinfo) ->
 	%% 1. Create the underlying tables for the content
 	%% 2. Update the record in the typeinfo table
 	%% 3. Create the default index for this type as well (using a util_emxindex:create_index call as we are already in the emx_admin context)
-ok.
+	mnesia:create_table(get_table_name(content, Type#emxtypeinfo.tableprefix), [
+			{ attributes, record_info(fields, tcontent) },
+			{ index, [] },
+			{ Type#emxtypeinfo.tabletype, [node() | nodes()] },
+			{ record_name, tcontent}
+			]
+			),
+	mnesia:create_table(get_table_name(header, Type#emxtypeinfo.tableprefix), [
+			{ attributes, record_info(fields, theader) },
+			{ index, [] },
+			{ Type#emxtypeinfo.tabletype, [node() | nodes()] },
+			{ record_name, theader}
+			]
+			),
 
+	mnesia:transaction(fun() -> mnesia:write(Type) end),
+	ok.
+
+get_table_name(content, Prefix) ->
+	list_to_atom(util_string:format("emxc~p", [Prefix]));
+get_table_name(header, Prefix) ->
+	list_to_atom(util_string:format("emxh~p", [Prefix])).
+	
