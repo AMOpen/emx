3790b6a1a7eb887137939f609866bb2a4c590c49
Alan Moore <amkimian@mac.com>
2009-02-22 08:30:39 -0800

    Actually commit the new check module

---
+++ src/util_check.erl
@@ -0,0 +1,15 @@
+-module(util_check).
+
+-export([check_message_queue/0]).
+
+-include("emx.hrl").
+
+check_message_queue() ->
+    {message_queue_len, Len } = erlang:process_info(self(), message_queue_len),
+    case Len > 100 of
+    	true ->
+    		?LOG(debug, "Message queue length is ~p", [ Len ]);
+	false ->
+		none
+    end.
+	
