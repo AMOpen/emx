7afd6797203ee35150fbdca36d4e641a2f5e2a91
Alan Moore <amkimian@mac.com>
2009-03-15 12:49:08 -0700

    Minor test changes

--- src/emx_test.erl
+++ src/emx_test.erl
@@ -1,23 +1,23 @@
 -module(emx_test).
 
--export([test/0]).
+-export([test/2]).
 
 -include_lib("emx.hrl").
 
-test() ->
+test(Count, Count2) ->
     application:start(emx),
     lists:foreach(fun (Id) ->
 			  io:format("Running for ~p~n", [Id]),
-			  run_for(util_string:format("official/system.~p",
+			  run_for(Count2, util_string:format("official/system.~p",
 						     [Id]))
 		  end,
-		  lists:seq(0, 10)).
+		  lists:seq(0, Count)).
 
-run_for(Prefix) ->
+run_for(Count2, Prefix) ->
     lists:foreach(fun (Id) ->
-			  emx_http:put_data("alan-moores-imac-2.local", 8665, util_string:format("~s/~p",
+			  emx_admin:put_data(util_string:format("~s/~p",
 							       [Prefix, Id]),
 					    util_string:format("<data>~p</data>",
 							       [Id]))
 		  end,
-		  lists:seq(0, 10)).
+		  lists:seq(0, Count2)).
